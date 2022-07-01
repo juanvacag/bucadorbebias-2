af128608d4a57eee55706a220c696cdca65f825e
juanvacag <>
2022-07-01 08:40:07 +0200

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Container } from 'react-bootstrap'
+
+function App() {
+
+  return (
+    <>
+      <header className="py-5">
+        <h1>Buscador de bebidas</h1>
+      </header>
+
+      <Container>
+        1
+      </Container>
+    </>
+  )
+}
+
+export default App
