99b0b52d0a53b568121520499ab2ec0d73aa8151
juanvacag <>
2022-07-01 12:24:18 +0200

    Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Container } from 'react-bootstrap'
 import Formulario from './components/Formulario'
+import {CategoriasProvider} from './context/CategoriasProvider'
 
 function App() {
 
   return (
-    <>
+    <CategoriasProvider>
       <header className="py-5">
         <h1>Buscador de bebidas</h1>
       </header>
@@ -12,7 +13,7 @@ function App() {
       <Container className='mt-5'>
         <Formulario />
       </Container>
-    </>
+    </CategoriasProvider>
   )
 }
 
