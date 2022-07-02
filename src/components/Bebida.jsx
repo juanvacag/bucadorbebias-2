c1b906660cc18948547fb8213658bd2c57dd0b2a
juanvacag <>
2022-07-02 18:53:25 +0200

    final

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,8 +1,33 @@
-
+import {Col, Card, Button } from 'react-bootstrap'
+import useBebidas from '../hooks/useBebidas'
 
 const Bebida = ({bebida}) => {
+
+    const {handleModalClick, handleBebidaIdClick} = useBebidas()
   return (
-    <div>{bebida.strDrink}</div>
+        <Col md={6} lg={3}>
+            <Card className='mb-4 mt-4'>
+                <Card.Img 
+                    variant='top'
+                    src={bebida.strDrinkThumb}
+                    alt={`Imagen de ${bebida.strDrink}`}
+                />
+                <Card.Body className='text-center'>
+                    <Card.Title>{bebida.strDrink}</Card.Title>
+                    {/* <Card.Text></Card.Text> */}
+                    <Button
+                        variant={'warning'}
+                        className='w-100 text-uppercase mt-2'
+                        onClick={() => {
+                            handleModalClick()
+                            handleBebidaIdClick(bebida.idDrink)
+                        }}
+                    >
+                        Ver Receta
+                    </Button>
+                </Card.Body>
+            </Card>
+        </Col>
   )
 }
 
--- src/context/BebidasProvider.jsx
+++ src/context/BebidasProvider.jsx
@@ -6,6 +6,31 @@ const BebidasContext = createContext()
 const BebidasProvider = ({children}) => {
 
     const [bebidas, setBebidas] = useState([])
+    const [modal, setModal] = useState(false) //State del Modal
+    const [bebidaId, setBebidaId] = useState(null) //State del Id
+    const [receta, setReceta] = useState({}) //State de la receta
+    const [cargando, setCargando] = useState(false)
+
+    useEffect(()=> {
+        setCargando(true)
+        const obtenerReceta = async () => {
+            if(!bebidaId) return
+
+            try {
+                const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${bebidaId}`
+
+                const {data} = await axios(url)
+                //console.log(data.drinks[0])
+                setReceta(data.drinks[0])
+
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setCargando(false)
+            }
+        }
+        obtenerReceta()
+    },[bebidaId])
 
     const consultarBebida = async datos => {
         try {
@@ -16,15 +41,28 @@ const BebidasProvider = ({children}) => {
             setBebidas(data.drinks)
 
         } catch (error) {
-            onslotchange.log(error)
+            console.log(error)
         }
+    }
+
+    const handleModalClick = () => {
+        setModal(!modal)
+    }
+
+    const handleBebidaIdClick = id => {
+        setBebidaId(id)
     }
 
     return (
         <BebidasContext.Provider
             value={{
                 consultarBebida,
-                bebidas
+                bebidas,
+                handleModalClick,
+                modal,
+                handleBebidaIdClick,
+                receta,
+                cargando
             }}
         >
             {children}
